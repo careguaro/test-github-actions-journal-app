6dd5dc0f1163c7fb5652b1a4726476303d066c69
careguaro <careguaro@gmail.com>
2023-10-29 01:55:45 +0200

    update getEnvironments helper to get env.test variables

--- src/helpers/getEnvironments.js
+++ src/helpers/getEnvironments.js
@@ -2,13 +2,25 @@
 export const getEnvironments = () => {
     // import.meta.env;
 
-    return {
-        // ...import.meta.env
-        VITE_APIKEY: import.meta.env.VITE_APIKEY,
-        VITE_AUTHDOMAIN: import.meta.env.VITE_AUTHDOMAIN,
-        VITE_PROJECTID: import.meta.env.VITE_PROJECTID,
-        VITE_STORAGEBUCKET: import.meta.env.VITE_STORAGEBUCKET,
-        VITE_MESSAGINGSENDERID: import.meta.env.VITE_MESSAGINGSENDERID,
-        VITE_APPID: import.meta.env.VITE_APPID,
+    if(import.meta.env.VITE_APIKEY) {
+        return {
+            // ...import.meta.env
+            VITE_APIKEY: import.meta.env.VITE_APIKEY,
+            VITE_AUTHDOMAIN: import.meta.env.VITE_AUTHDOMAIN,
+            VITE_PROJECTID: import.meta.env.VITE_PROJECTID,
+            VITE_STORAGEBUCKET: import.meta.env.VITE_STORAGEBUCKET,
+            VITE_MESSAGINGSENDERID: import.meta.env.VITE_MESSAGINGSENDERID,
+            VITE_APPID: import.meta.env.VITE_APPID,
+        }
+    } else {
+        return {
+            // ...import.meta.env
+            VITE_APIKEY: import.meta.env.test.VITE_APIKEY,
+            VITE_AUTHDOMAIN: import.meta.env.test.VITE_AUTHDOMAIN,
+            VITE_PROJECTID: import.meta.env.test.VITE_PROJECTID,
+            VITE_STORAGEBUCKET: import.meta.env.test.VITE_STORAGEBUCKET,
+            VITE_MESSAGINGSENDERID: import.meta.env.test.VITE_MESSAGINGSENDERID,
+            VITE_APPID: import.meta.env.test.VITE_APPID,
+        }
     }
 }
